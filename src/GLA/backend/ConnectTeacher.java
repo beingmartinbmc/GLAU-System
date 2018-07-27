25c661fe84e1a03b20f8a87e4cab1b88acb93d90
Ankit Sharma <beingbmc12@gmail.com>
2018-07-27 16:29:59 +0530

    Add files via upload
    Optimised Codes

--- src/GLA/backend/ConnectTeacher.java
+++ src/GLA/backend/ConnectTeacher.java
@@ -5,17 +5,17 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class Connectivity {
-
+public class ConnectTeacher {
     final static String url="jdbc:mysql://localhost:3306/Aliens";
     final static String username="root";
     final static String pass="yoyobmc12";
-    public static String []name=new String[5];
-    public static String []id=new String[5];
-    public static String []path=new String[5];
-    public static String []email=new String[5];
-    public static String []phone=new String[5];
-    public static String []password=new String[5];
+    public static String []id=new String[2];
+    public static String []name1=new String[2];
+    public static String []name2=new String[2];
+    public static String []name3=new String[2];
+    public static String []name4=new String[2];
+    public static String []name5=new String[2];
+    public static String []name6=new String[2];
     public static int index;
 
     static Connection connection;
@@ -25,21 +25,21 @@ public class Connectivity {
     public static void fetchDetails()throws Exception{
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection=DriverManager.getConnection(url,username,pass);
-        String query="select * from student";
+        String query="select * from faculty";
         statement=connection.prepareStatement(query);
         resultSet=statement.executeQuery();
         int i=0;
         while(resultSet.next()){
-            name[i]=resultSet.getString("name");
-            id[i]=resultSet.getString("id");
-            phone[i]=resultSet.getString("phone");
-            email[i]=resultSet.getString("email");
-            path[i]=resultSet.getString("path");
-            password[i]=resultSet.getString("password");
+            id[i]=resultSet.getString("StudentID");
+            name1[i]=resultSet.getString("teacher1");
+            name2[i]=resultSet.getString("teacher2");
+            name3[i]=resultSet.getString("teacher3");
+            name4[i]=resultSet.getString("teacher4");
+            name5[i]=resultSet.getString("teacher5");
+            name6[i]=resultSet.getString("teacher6");
             i++;
         }
         connection.close();
         statement.close();
     }
 }
-
